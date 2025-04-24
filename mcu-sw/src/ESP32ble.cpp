f396c445f37fc5b05a7da7a8251b5991262fadd6
Philipp Winterberg <philippstefanjakob@gmail.com>
2025-04-24 02:22:51 +0200

    added Wheel and Speed Control

--- mcu-sw/src/ESP32ble.cpp
+++ mcu-sw/src/ESP32ble.cpp
@@ -47,7 +47,7 @@ class ServerCallbacks : public BLEServerCallbacks {
 };
 
 String ESP32ble::getCmd() const {
-    return "Speed_1: " + String(_speed1) + ", Speed_2: " + String(_speed2);
+    return "Speed_1: " + String(_speed1) + ", Speed_2: " + String(_speed2) + "Wheel_1: " + String(_wheel1) + ", Wheel_2: " + String(_wheel2);
 }
 
 void ESP32ble::setCmd(String value) {
